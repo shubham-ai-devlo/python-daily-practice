f89c711ac1bd039ba04628f59dc22ca55da6dec5
shubham-ai-devlo <shubhukumardhariwal@gmail.com>
2026-01-14 22:34:45 +0530

    Add files via upload
    Day 2: Variables practice added

---
+++ day2_variables.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+learning = input("What are you learning: ")
+print()
+print("Name:",name)
+print("Age:",age)
+print("Learning:",learning)
